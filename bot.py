811b2d1a56d34fae04056f698acef88f7be851fc
RINCEMON REJI <70466997+RincemonReji@users.noreply.github.com>
2021-03-03 14:59:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -76,7 +76,7 @@ async def start(bot, cmd):
 					reply_markup=InlineKeyboardMarkup(
 						[
 							[
-								InlineKeyboardButton("🤖 Join Updates Channel", url=f"https://t.me/joinchat/U1zAAZzvU8yOvW17")
+								InlineKeyboardButton("🤖 Join Updates Channel", url=f"https://t.me/ft_new_movie")
 							]
 						]
 					),
